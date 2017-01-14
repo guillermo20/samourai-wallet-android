7fa36506cf3ef5d9a7ada356b49b152680900f22
Guillermo Gutierrez <guillermo20@gmail.com>
2017-01-14 02:36:37 -0500

    some basic changes for the next UI

---
+++ app/src/main/java/com/samourai/wallet/CreateWalletActivity.java
@@ -0,0 +1,16 @@
+package com.samourai.wallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by guillermo on 1/14/17.
+ */
+
+public class CreateWalletActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_wallet_layout);
+    }
+}
