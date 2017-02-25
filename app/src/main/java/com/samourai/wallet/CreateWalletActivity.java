b8f36a41048275b781a6b2d6a838d004206a3ac0
Guillermo Gutierrez <guillermo20@gmail.com>
2017-02-25 18:21:37 -0500

    added passphrase layout and some resources.

--- app/src/main/java/com/samourai/wallet/CreateWalletActivity.java
+++ app/src/main/java/com/samourai/wallet/CreateWalletActivity.java
@@ -11,6 +11,6 @@ public class CreateWalletActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_wallet);
+        setContentView(R.layout.activity_passphrase);
     }
 }
